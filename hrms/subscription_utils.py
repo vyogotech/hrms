3a17ab80c3b7d122fe6113c4dfece1a2adf595d5
Varun Krishnamurthy <varkrish@redhat.com>
2025-03-17 11:28:36 +1100

    Adding Open

--- hrms/subscription_utils.py
+++ hrms/subscription_utils.py
@@ -173,8 +173,10 @@ def hide_erpnext() -> bool:
 
 
 def has_subscription(secret_key) -> bool:
-	url = f"https://frappecloud.com/api/method/press.api.developer.marketplace.get_subscription_status?secret_key={secret_key}"
-	response = requests.request(method="POST", url=url, timeout=5)
+    #Be Open :)
+	# url = f"https://frappecloud.com/api/method/press.api.developer.marketplace.get_subscription_status?secret_key={secret_key}"
+	# response = requests.request(method="POST", url=url, timeout=5)
 
-	status = response.json().get("message")
-	return True if status == "Active" else False
+	# status = response.json().get("message")
+	# return True if status == "Active" else False
+	return True
